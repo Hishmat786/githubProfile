6d1776cb11c2764231bb9ae69208984f730d34f7
Hishmat786 <hishmatdrawarh786@gmail.com>
2024-03-22 23:21:24 +0500

    github data complete with profile

--- script.js
+++ script.js
@@ -1,39 +1,74 @@
-const sub= document.getElementById("submit")
+
+const btn = document.getElementById("submit")
+let profille = ""
+function addData() {
+    // alert("clicked")
+    const name = document.getElementById("name").value;
+    const mail = document.getElementById("email").value;
+    const git = document.getElementById("git").value;
+
+    if (name && mail && git) {
+        const div = document.createElement("div")
+        div.className = "div"
+        div.style.display = "flex";
+        div.style.flexDirection = "row";
+        div.style.justifyContent = "space-between"
+        div.style.padding = "10px"
+        div.style.border = "2px solid black";
+
+        fetch(`https://api.github.com/users/${git}`)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                profille = data.avatar_url;
+            })
+            .catch(error => {
+                alert("No account")
+            });
 
 
-function addProfile(){
-    
-    const name = document.getElementById("name").value
-    const email = document.getElementById("email").value
-    const github = document.getElementById("account").value
 
-    const prof= document.getElementById("data")
+        const pro = document.createElement("img")
+        pro.src = profille
+        pro.alt = "profile"
 
-    const profile= document.createElement("div")
-    profile.className = "profi";
-   
-    var namePara = document.createElement("p");
-    namePara.textContent = "Name: " + name;
-        
-    var emailPara = document.createElement("p");
-    emailPara.textContent = "Email: " + email;
-        
-    var githubPara = document.createElement("p");
-    githubPara.textContent = "GitHub Username: " + github;
-        
-    var gotoButton = document.createElement("button");
-    gotoButton.textContent = "Go to User";
-    gotoButton.addEventListener("click", function() {
-        console.log("Name: " + name + "\nEmail: " + email + "\nGitHub Username: " + github);
-    });
+        pro.style.width = "50px"
+        pro.style.height = "50px"
+        const pName = document.createElement("p")
+        pName.textContent = name
+        const pmail = document.createElement("p")
+        pmail.textContent = mail
+        const pgit = document.createElement("p")
+        pgit.textContent = git
 
-    div.appendChild(namePara);
-    div.appendChild(emailPara);
-    div.appendChild(githubPara);
-    div.appendChild(gotoButton);
-        
-    outputSection.appendChild(div);
+        const showbtn = document.createElement("button")
+        showbtn.textContent = "Show"
+        showbtn.style.width = "50px"
+        showbtn.style.height = "25px"
+        showbtn.style.marginTop = "10px"
+        showbtn.style.backgroundColor = "blue-light"
 
+
+        showbtn.addEventListener("click", function () {
+            window.location.href = `https:www.github.com/${git}`
+        });
+
+        div.appendChild(pro)
+        div.appendChild(pName)
+        div.appendChild(pmail)
+        div.appendChild(pgit)
+        div.appendChild(showbtn)
+
+        const info = document.getElementById("area")
+
+        info.appendChild(div)
+    } else {
+        alert("Don't Miss any field")
+    }
 }
 
-sub.onclick = addProfile
+btn.onclick = addData
